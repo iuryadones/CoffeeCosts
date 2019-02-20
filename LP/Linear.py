43c40f2f8902bc69dc19b92e37828c9143509723
Patricia Lessa <patriciaslessa7@gmail.com>
2019-02-20 10:34:05 -0300

    organiza codigo.

--- LP/Linear.py
+++ LP/Linear.py
@@ -3,16 +3,16 @@ import numpy as np
 
 #O padrão de entrada dos dados deve ser feita para um problema de maximização
 
-# c = np.array([-3, -5])
-# A = np.array([[1,0], [0,1], [3,2]])
-# b = np.array([4, 6, 18])
-# res = linprog(c, A, b)
-# print(res)
-# print(f'\n DUAL \n')
-# A_T_n = -A.transpose()
-# resd = linprog(-b, A_T_n, -c)
-# print(f' Os multplicadores de lagrange são:')
-# print(resd)
+c = np.array([-3, -5])
+A = np.array([[1,0], [0,1], [3,2]])
+b = np.array([4, 6, 18])
+res = linprog(c, A, b, options={'maxiter':10000})
+print(dir(res))
+print(f'\n DUAL \n')
+A_T_n = -A.transpose()
+resd = linprog(-b, A_T_n, -c, options={'maxiter':10000})
+print(f' Os multplicadores de lagrange são:')
+print(resd)
 # #
 ####################################################################
 
@@ -36,15 +36,15 @@ import numpy as np
 # print(resd)
 
 
-c = np.array([-40, -60, -20])
-A = np.array([[80, 30, 10], [40, 100, 60], [20, 70, 90]])
-b = np.array([800, 1000, 600])
-res = linprog(c, A, b)
-print(res)
-print(f'\n DUAL \n')
-A_T_n = -A.transpose()
-resd = linprog(-b, A_T_n, c)
-print(f' Os multplicadores de lagrange são:')
-print(resd)
+# c = np.array([-40, -60, -20])
+# A = np.array([[80, 30, 10], [40, 100, 60], [20, 70, 90]])
+# b = np.array([800, 1000, 600])
+# res = linprog(c, A, b)
+# print(res)
+# print(f'\n DUAL \n')
+# A_T_n = -A.transpose()
+# resd = linprog(-b, A_T_n, c)
+# print(f' Os multplicadores de lagrange são:')
+# print(resd)
 #
 #
