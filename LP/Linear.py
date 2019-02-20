73f5010fc866a74354fec5ec834fbccf65d369be
Fernando Campello <fernandocampello7@gmail.com>
2019-02-20 05:26:04 -0800

    Ultima aula 13022019

--- LP/Linear.py
+++ LP/Linear.py
@@ -1,16 +1,23 @@
-from scipy.optimize import linprog
+from scipy.optimize import (
+    linprog,
+    linprog_verbose_callback)
 import numpy as np
+
 
 #O padrão de entrada dos dados deve ser feita para um problema de maximização
 
 # c = np.array([-3, -5])
 # A = np.array([[1,0], [0,1], [3,2]])
 # b = np.array([4, 6, 18])
-# res = linprog(c, A, b)
+# res = linprog(c, A, b,
+#               options={'maxiter':10000})
 # print(res)
+# linprog_verbose_callback(res)
 # print(f'\n DUAL \n')
 # A_T_n = -A.transpose()
-# resd = linprog(-b, A_T_n, -c)
+# resd = linprog(-b, A_T_n, c,
+#               options={'maxiter':10000}
+#               )
 # print(f' Os multplicadores de lagrange são:')
 # print(resd)
 # #
@@ -22,8 +29,9 @@ import numpy as np
 #     print(f' Os multplicadores de lagrange são:{resd}')
 # else:
 #     resd = linprog(b, Ad, -c)
-#     print(f' Os multplicadores de lagrange são:{resd}')
-
+#     print(f' Os
+#multplicadores de lagrange são:{resd}')
+#
 # c = np.array([4, 6, 18])
 # A = np.array([[-1, 0, -3],[0, -1, -2]])
 # b = np.array([-3, -5])
@@ -31,6 +39,7 @@ import numpy as np
 # print(res)
 # print(f'\n DUAL \n')
 # A_T = -A.transpose()
+#
 # resd = linprog(-b, A_T, c)
 # print(f' Os multplicadores de lagrange são:')
 # print(resd)
@@ -43,8 +52,8 @@ res = linprog(c, A, b)
 print(res)
 print(f'\n DUAL \n')
 A_T_n = -A.transpose()
-resd = linprog(-b, A_T_n, c)
-print(f' Os multplicadores de lagrange são:')
+resd = linprog(b, A_T_n, c)
+print(f' Os multplicadores de Lagrange são:')
 print(resd)
 #
 #
